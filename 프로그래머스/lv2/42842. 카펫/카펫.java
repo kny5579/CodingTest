fad6b1d212603aeed9dae26897292b114dc7d41a
나연 <95288763+kny5579@users.noreply.github.com>
2023-09-19 17:11:55 +0900

    [level 2] Title: 카펫, Time: 0.02 ms, Memory: 73.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42842. 카펫/카펫.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] solution(int brown, int yellow) {
+        int[] answer = new int[2];
+        int sum=brown+yellow;
+        for (int i = 1; i < sum/2; i++) {
+            if(sum%i==0&&(i-2)*((sum/i)-2)==yellow){
+                answer[0]=i;
+                answer[1]=sum/i;
+            }
+        }
+        return answer;
+    }
+}
