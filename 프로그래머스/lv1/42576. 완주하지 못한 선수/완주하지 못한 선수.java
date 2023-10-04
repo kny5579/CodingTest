c9438fd1f99a97b7fa2a6ccf26fef44827a9c3cb
나연 <95288763+kny5579@users.noreply.github.com>
2023-10-04 15:19:50 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 65.57 ms, Memory: 94.1 MB -BaekjoonHub

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,20 +1,6 @@
-import java.util.HashMap;
-
 class Solution {
     public String solution(String[] participant, String[] completion) {
         String answer = "";
-        HashMap<String,Integer> map=new HashMap<>();
-        for (int i = 0; i < participant.length; i++) {
-            map.put(participant[i],map.getOrDefault(participant[i],0)+1);
-        }
-        for (int i = 0; i < completion.length; i++) {
-            map.put(completion[i],map.get(completion[i])-1);
-        }
-        for (String k:map.keySet()){
-            if(map.get(k)>0){
-                return k;
-            }
-        }
         return answer;
     }
 }
