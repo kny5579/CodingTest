42b092f7fee8ef9c3e5c7c916d0f540af015e257
나연 <95288763+kny5579@users.noreply.github.com>
2025-01-19 17:08:13 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 13.52 ms, Memory: 89.4 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -1,18 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+
 class Solution {
     public String solution(int[] food) {
-        String answer = "";
-	        for(int i=1;i<food.length;i++){
-	        	if(food[i]!=1) {
-	        		for(int k=0;k<food[i]/2;k++) {
-	        			answer+=Integer.toString(i);
-	        		}
-	        	}
-	        }
-	        answer+="0";
-            //reverse
-	        for(int i=answer.length()-2;i>=0;i--) {
-	        	answer+=answer.charAt(i);
-	        }
-        return answer;
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i < food.length; i++) {
+            if(food[i] == 1) continue;
+            for (int j = 0; j < food[i]/2; j++) {
+                sb.append(i);
+            }
+        }
+        
+        return sb.toString()+"0"+sb.reverse().toString();
     }
 }
