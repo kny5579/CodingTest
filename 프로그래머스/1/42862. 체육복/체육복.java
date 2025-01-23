7f9d56286f382312a9da70a90e732fd822cdf621
나연 <95288763+kny5579@users.noreply.github.com>
2025-01-23 17:44:44 +0900

    [level 1] Title: 체육복, Time: 0.03 ms, Memory: 73.1 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.java
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -1,37 +1,25 @@
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.stream.Collectors;
-
 class Solution {
-    public static int solution(int n, int[] lost, int[] reserve) {
-        Arrays.sort(lost);
-        LinkedList<Integer> reserveList = Arrays.stream(reserve)
-                                    .boxed().collect(Collectors.toCollection(LinkedList::new));
+    public int solution(int n, int[] lost, int[] reserve) {
+        int[] people = new int[n];
+        int answer = n;
 
-        int answer = 0;
+        for (int l : lost) 
+            people[l-1]--;
+        for (int r : reserve) 
+            people[r-1]++;
 
-        for(int i = 0; i<lost.length; i++) {
-            int index = i;
-            if (reserveList.contains(lost[i])) {
-                reserveList.removeIf(val -> val == lost[index]);
-                lost[i] = -2;
-                answer++;
+        for (int i = 0; i < people.length; i++) {
+            if(people[i] == -1) {
+                if(i-1>=0 && people[i-1] == 1) {
+                    people[i]++;
+                    people[i-1]--;
+                }else if(i+1< people.length && people[i+1] == 1) {
+                    people[i]++;
+                    people[i+1]--;
+                }else 
+                    answer--;
             }
         }
-
-        for(int i = 0; i<lost.length; i++) {
-            int index = i;
-            if(reserveList.contains(lost[i] - 1)) {
-                reserveList.removeIf(val -> val == lost[index] - 1);
-                answer++;
-                continue;
-            }
-            if(reserveList.contains(lost[i] + 1)) {
-                reserveList.removeIf(val -> val == lost[index] + 1);
-                answer++;
-            }
-        }
-
-        return n - lost.length + answer;
+        return answer;
     }
 }
