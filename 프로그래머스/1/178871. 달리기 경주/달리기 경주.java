05fc3013d42321ad119b6daf87ddca6e894dc40e
나연 <95288763+kny5579@users.noreply.github.com>
2025-03-23 15:49:06 +0900

    [level 1] Title: 달리기 경주, Time: 313.72 ms, Memory: 372 MB -BaekjoonHub

--- 프로그래머스/1/178871. 달리기 경주/달리기 경주.java
+++ 프로그래머스/1/178871. 달리기 경주/달리기 경주.java
@@ -1,24 +1,22 @@
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
 
 class Solution {
     public String[] solution(String[] players, String[] callings) {
-        String[] answer = players.clone();
-        HashMap<String,Integer> map = new HashMap<>();
+        HashMap<String,Integer> map=new HashMap<>();
         for (int i = 0; i < players.length; i++) {
             map.put(players[i],i);
         }
-        
+
         for (int i = 0; i < callings.length; i++) {
-            int idx = map.get(callings[i]);
-            String temp = answer[idx-1];
-            answer[idx]=temp;
-            answer[idx-1]=callings[i];
+            int idx=map.get(callings[i]);
+            String tmp=players[idx-1];
+            players[idx]=tmp;
+            players[idx-1]=callings[i];
+            map.put(tmp,idx);
             map.put(callings[i],idx-1);
-            map.put(temp,idx);
         }
-        return answer;
+
+        return players;
     }
 }
