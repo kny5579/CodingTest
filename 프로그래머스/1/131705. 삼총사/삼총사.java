0cec5124983c14e0044e8530434966924b265947
나연 <95288763+kny5579@users.noreply.github.com>
2025-04-06 22:37:22 +0900

    [level 1] Title: 삼총사, Time: 0.04 ms, Memory: 77 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/삼총사.java
+++ 프로그래머스/1/131705. 삼총사/삼총사.java
@@ -1,12 +1,13 @@
 class Solution {
     public int solution(int[] number) {
-        int answer = 0;
-        for (int i = 0; i < number.length-2; i++) {
-            for (int j = i+1; j < number.length-1; j++) {
-                for (int k = j+1; k < number.length; k++) {
-                    if(number[i]+number[j]+number[k]==0) answer++;
-                }
-            }
+        int answer=0;
+        int i,j,k;
+        for(i=0;i<number.length;i++) {
+        	for(j=i+1;j<number.length;j++) {
+        		for(k=j+1;k<number.length;k++) {
+        			if(number[i]+number[j]+number[k]==0) answer++;
+        		}
+        	}
         }
         return answer;
     }
