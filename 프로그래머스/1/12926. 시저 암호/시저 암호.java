b42543bed92ddf733c74a08da1634c40e0041ce8
나연 <95288763+kny5579@users.noreply.github.com>
2025-05-12 14:46:49 +0900

    [level 1] Title: 시저 암호, Time: 44.11 ms, Memory: 111 MB -BaekjoonHub

--- 프로그래머스/1/12926. 시저 암호/시저 암호.java
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.java
@@ -2,16 +2,25 @@ class Solution {
     public String solution(String s, int n) {
         String answer = "";
         for (int i = 0; i < s.length(); i++) {
-            char temp = s.charAt(i);
-            if(temp == ' ') answer+=" ";
-            else {
-                if(Character.isLowerCase(temp)) {
-                    if(temp+n<='z') answer+=(char)(temp+n);
-                    else answer+=(char)(temp-26+n);
-                } else if(Character.isUpperCase(temp)) {
-                    if(temp+n<='Z') answer+=(char)(temp+n);
-                    else answer+=(char)(temp-26+n);
+            char ex=s.charAt(i);
+            if(ex==' '){
+                answer+=" ";
+            }
+            else if(Character.isLowerCase(ex)){
+                if(ex+n>'z'){
+                    answer+=(char)(ex+n-26);
+                } else{
+                    answer+=(char)(ex+n);
                 }
+            }
+            else {
+                if(ex+n>'Z'){
+                    answer+=(char)(ex+n-26);
+                }
+                else{
+                    answer+=(char)(ex+n);
+                }
+
             }
         }
         return answer;
