510f3782ffab203c0a519dc319af965833b2d021
나연 <95288763+kny5579@users.noreply.github.com>
2025-06-03 18:59:30 +0900

    [Silver II] Title: 팰린드롬 만들기, Time: 124 ms, Memory: 14272 KB -BaekjoonHub

---
+++ 백준/Silver/1254. 팰린드롬 만들기/팰린드롬 만들기.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        String str = st.nextToken(); // 문자열
+
+        int cnt = 0; // 추가된 문자의 개수
+        int left = 0;
+        while (true) {
+            // 팰린드롬 확인
+            boolean flag = isPalindrome(left, str.length() - 1, str.toCharArray());
+            if (flag) {
+                cnt = left + str.length();
+                break;
+            }
+            left++;
+        }
+        System.out.println(cnt);
+    }
+
+    public static boolean isPalindrome(int left, int right, char[] arr) {
+        while (left < right) {
+            // 같지 않을 경우
+            if (arr[left] != arr[right]) {
+                break;
+            }
+            left++;
+            right--;
+        }
+
+        if (left >= right) {
+            return true;
+        }
+        return false;
+    }
+}
