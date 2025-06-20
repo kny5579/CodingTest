4baf9dd8c4f733ffb87a0aaab2ea0dc725cb8b09
나연 <95288763+kny5579@users.noreply.github.com>
2025-06-20 22:30:14 +0900

    [level 0] Title: 각도기, Time: 0.02 ms, Memory: 71.8 MB -BaekjoonHub

--- 프로그래머스/0/120829. 각도기/각도기.java
+++ 프로그래머스/0/120829. 각도기/각도기.java
@@ -1,10 +1,8 @@
 class Solution {
     public int solution(int angle) {
-        int answer = 0;
-        if(angle<90) answer=1;
-        else if(angle==90) answer=2;
-        else if(angle==180) answer=4;
-        else answer=3;
-        return answer;
+        if(angle>0&&angle<90) return 1;
+        else if(angle==90) return 2;
+        else if(angle>90&&angle<180) return 3;
+        else return 4;
     }
 }
