cf7db60dd130172665937fe9a1aab39a5f572962
나연 <95288763+kny5579@users.noreply.github.com>
2025-09-15 18:23:12 +0900

    [level 1] Title: 명예의 전당 (1), Time: 13.96 ms, Memory: 91.1 MB -BaekjoonHub

--- 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
@@ -1,20 +1,25 @@
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.List;
 
 class Solution {
     public int[] solution(int k, int[] score) {
         int[] answer = new int[score.length];
-        List<Integer> list = new ArrayList<>();
-        for (int i = 0; i < answer.length; i++) {
-            list.add(score[i]);
-            Collections.sort(list);
-            if(list.size()>k) {
-                list.remove(0);
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = 0; i < score.length; i++) {
+            if(i<k){
+                list.add(score[i]);
+                Collections.sort(list);
+                answer[i]=list.get(0);
+                continue;
             }
-            answer[i] = list.get(0);
+            Collections.sort(list);
+            if(list.size()==k && list.get(0)<score[i]) {
+                list.remove(0);
+                list.add(score[i]);
+            }
+            Collections.sort(list);
+            answer[i]=list.get(0);
         }
-
         return answer;
     }
 }
